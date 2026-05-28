972e3512db09bc615827b84672aab12ffac9e039
aqhbardan14l <aqhbardan14l@gmail.com>
2026-05-29 02:29:30 +0800

    Create knowledgeBase.js

---
+++ knowledgeBase.js
@@ -0,0 +1,18 @@
+const KNOWLEDGE_BASE = {
+  maxSentenceLength: 15,
+
+  rules: [
+    "Use simple and direct language",
+    "Break long text into chunks",
+    "Use bullet points",
+    "Avoid technical jargon",
+    "Avoid figurative language",
+    "Explain difficult words",
+    "Put important ideas first",
+    "Use supportive tone",
+    "Reduce cognitive overload",
+    "Use short sentences",
+    "Repeat key ideas only",
+    "Use examples when needed"
+  ]
+};
