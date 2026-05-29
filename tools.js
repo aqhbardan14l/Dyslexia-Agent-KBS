76bac83a9fc8dce343f032de6b5f38288aa0d72c
aqhbardan14l <aqhbardan14l@gmail.com>
2026-05-29 15:46:15 +0800

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,42 +1,8 @@
 function runAgent() {
 
-    let input = document.getElementById("userInput").value.toLowerCase();
+    alert("YES BERJAYA BUTTON CLICKED");
 
-    let response = "";
+    let input = document.getElementById("userInput").value;
 
-    if (input.includes("dyslexia")) {
-
-        response = `
-        Dyslexia affects reading.<br>
-        Use short sentences.<br>
-        Break information into parts.
-        `;
-
-    } else if (input.includes("study")) {
-
-        response = `
-        Study one topic at a time.<br>
-        Use bullet points.<br>
-        Take short breaks.
-        `;
-
-    } else if (input.includes("stress")) {
-
-        response = `
-        Take deep breaths.<br>
-        Stay calm.<br>
-        Do one task at a time.
-        `;
-
-    } else {
-
-        response = `
-        Please use keywords:<br>
-        - dyslexia<br>
-        - study<br>
-        - stress
-        `;
-    }
-
-    document.getElementById("outputText").innerHTML = response;
+    document.getElementById("outputText").innerHTML = input;
 }
